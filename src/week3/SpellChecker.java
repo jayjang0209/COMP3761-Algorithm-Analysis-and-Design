e06e2816a3115995b1591214af5043cf5c5a1ce5
Jonghoon Jang <jayjang0209@gmail.com>
2022-02-06 09:55:41 -0800

    Add biSearch solution

--- src/week3/SpellChecker.java
+++ src/week3/SpellChecker.java
@@ -7,97 +7,117 @@ import java.util.Scanner;
 
 public class SpellChecker {
 
-	/*
-	 * Returns an ArrayList of words from a file.
-	 */
-	static ArrayList<String> read(String file) throws FileNotFoundException
-	{
-		ArrayList<String> list 	= new ArrayList<String>();
-		Scanner reader			= new Scanner(System.in);
+    /*
+     * Returns an ArrayList of words from a file.
+     */
+    static ArrayList<String> read(String file) throws FileNotFoundException {
+        ArrayList<String> list = new ArrayList<String>();
+        Scanner reader = new Scanner(System.in);
 
-		reader = new Scanner(new FileReader(file));
-		
-		while(reader.hasNext()) // reads the file until end of file is reached
-			list.add(reader.next()); //stores these words in an array
+        reader = new Scanner(new FileReader(file));
 
-		reader.close();
+        while (reader.hasNext()) // reads the file until end of file is reached
+            list.add(reader.next()); //stores these words in an array
 
-		return list;
-	}
+        reader.close();
 
-	/*
-	 * Sequential Search or Linear Search is a brute force technique which forces
-	 * the program to loop through each index until the key value is found.
-	 * Normally the technique would have an efficiency of O(n) if the program is looking
-	 * for a single key. However, in this case the efficiency should be O(n^2) as we are
-	 * comparing 2 different ArrayLists and the outer loop is dependent of the inner loop.
-	 * Efficiency : O(n^2)
-	 */
-	static void seqSearch(ArrayList<String> test, ArrayList<String> words)
-	{
-		double startTime, elapsedTime;
-		ArrayList <String> wordsNotFound = new ArrayList<String>();
-		int count = 0;
+        return list;
+    }
 
-		startTime = System.currentTimeMillis();
-		System.out.println(test.size());
-		System.out.println(words.size());
+    /*
+     * Sequential Search or Linear Search is a brute force technique which forces
+     * the program to loop through each index until the key value is found.
+     * Normally the technique would have an efficiency of O(n) if the program is looking
+     * for a single key. However, in this case the efficiency should be O(n^2) as we are
+     * comparing 2 different ArrayLists and the outer loop is dependent of the inner loop.
+     * Efficiency : O(n^2)
+     */
+    static void seqSearch(ArrayList<String> test, ArrayList<String> words) {
+        double startTime, elapsedTime;
+        ArrayList<String> wordsNotFound = new ArrayList<String>();
+        int count = 0;
 
-		for (int i = 0; i < test.size(); i++) {
+        startTime = System.currentTimeMillis();
 
-			boolean isWordFound = false;
+        /* Implementation */
+        for (int i = 0; i < test.size(); i++) {
 
-			for (int j = 0;  j < words.size(); j++) {
-				if (test.get(i).equalsIgnoreCase(words.get(j))) { // basic operation O(n^2)
-					isWordFound = true;
-				}
-			}
+            boolean isWordFound = false;
 
-			if (!isWordFound) {
-				wordsNotFound.add(test.get(i));
-				count++;
-			}
-		}
+            for (int j = 0; j < words.size(); j++) {
+                if (test.get(i).equalsIgnoreCase(words.get(j))) { // basic operation O(n^2)
+                    isWordFound = true;
+                }
+            }
 
-		elapsedTime = (System.currentTimeMillis() - startTime) * 1000;
-		System.out.println("Sequential Search:\t" + count + " words not found. " + elapsedTime + " microseconds.");
-		System.out.println("Words not found:\t" + wordsNotFound);
-	}
+            if (!isWordFound) {
+                wordsNotFound.add(test.get(i));
+                count++;
+            }
+        }
 
-	/*
-	 *  Binary search is a method in which the 'middle' index of reference array
-	 *  and the 'key' value is always compared. In each comparison, the middle
-	 *  index adjusts depending if the key is < or > or = to the middle.
-	 *  Looping through the test_data is (n) times, and comparing it using
-	 *  binary search technique is (logn).Thus, program is supposed to run (n)(logn) times
-	 *  Efficiency : O(nlogn)
-	 */
-	static void binSearch(ArrayList<String> test, ArrayList<String> words)
-	{
-		double startTime, elapsedTime;
-		ArrayList <String> wordsNotFound	= new ArrayList<String>();
-		int count = 0;
+        elapsedTime = (System.currentTimeMillis() - startTime) * 1000;
+        System.out.println("Sequential Search:\t" + count + " words not found. " + elapsedTime + " microseconds.");
+        System.out.println("Words not found:\t" + wordsNotFound);
+    }
 
-		startTime = System.currentTimeMillis();
-		    
-		    //Implement binary Search
+    /*
+     *  Binary search is a method in which the 'middle' index of reference array
+     *  and the 'key' value is always compared. In each comparison, the middle
+     *  index adjusts depending if the key is < or > or = to the middle.
+     *  Looping through the test_data is (n) times, and comparing it using
+     *  binary search technique is (logn).Thus, program is supposed to run (n)(logn) times
+     *  Efficiency : O(nlogn)
+     */
+    static void binSearch(ArrayList<String> test, ArrayList<String> words) {
+        double startTime, elapsedTime;
+        ArrayList<String> wordsNotFound = new ArrayList<String>();
+        int count = 0;
 
-		elapsedTime = (System.currentTimeMillis() - startTime) * 1000;
-		System.out.println("Binary Search:\t\t" + count + " words not found. " + elapsedTime + " microseconds.");
-		System.out.println("Words not found:\t" + wordsNotFound);
-	}
+        startTime = System.currentTimeMillis();
 
-	/*
-	 * main method
-	 */
-	public static void main(String[] args) throws FileNotFoundException
-	{
-		ArrayList <String> test		= read("lab3_testdata.txt");
-		ArrayList <String> words	= read("lab3_wordlist.txt");
+        /* Implementation */
+        for (int i = 0; i < test.size(); i++) {
+            int left = 0;
+            int right = words.size() - 1;
+            Boolean found = false;
+
+            while (!found && left <= right) {
+                int mid = (left + right) / 2;
+                String elementToCompare = words.get(mid);
+//                System.out.println(elementToCompare);
+                if (elementToCompare.equalsIgnoreCase(test.get(i))) {
+                    found = true;
+                } else if (test.get(i).compareToIgnoreCase(elementToCompare) < 0) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            if (!found) {
+                wordsNotFound.add(test.get(i));
+                count++;
+            }
+
+        }
+
+
+        elapsedTime = (System.currentTimeMillis() - startTime) * 1000;
+        System.out.println("Binary Search:\t\t" + count + " words not found. " + elapsedTime + " microseconds.");
+        System.out.println("Words not found:\t" + wordsNotFound);
+    }
+
+    /*
+     * main method
+     */
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<String> test = read("lab3_testdata.txt");
+        ArrayList<String> words = read("lab3_wordlist.txt");
 
 		seqSearch(test, words);
-		System.out.println();
-//		binSearch(test,words);
-	}
+        System.out.println();
+        binSearch(test, words);
+    }
 
 }

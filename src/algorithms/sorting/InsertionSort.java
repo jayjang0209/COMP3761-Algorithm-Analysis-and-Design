f97d3596716507cfe00563f40aab95716af3f6cc
Jonghoon Jang <jayjang0209@gmail.com>
2022-02-07 22:06:28 -0800

    Implement Insertion sort Recursive

--- src/algorithms/sorting/InsertionSort.java
+++ src/algorithms/sorting/InsertionSort.java
@@ -9,7 +9,7 @@ public class InsertionSort {
             int key = array[i];
             int j = i - 1;
 
-            while (j >= 0 && array[j] > key) {
+            while (j >= 0 && array[j] > key) { // basic operation O(n^2)
                 array[j + 1] = array[j];
                 j = j - 1;
             }
@@ -18,13 +18,24 @@ public class InsertionSort {
     }
 
 
-    public static void insertionSortRecursive(final Integer[] array) {
+    public static void insertionSortRecursive(final Integer[] array, final int length) {
+        if (length > 1) {
+            insertionSortRecursive(array, length - 1);
+            int key = array[length - 1];
+            int i = length - 2;
+            while (i >= 0 && array[i] > key) {
+                array[i + 1] = array[i];
+                i = i - 1;
+            }
+            array[i + 1] = key;
+        }
+
 
     }
 
     public static void main(String[] args) {
         Integer[] array = { 2, 1, 4, 6, 3, 5 };
-        insertionSortIteratively(array);
+        insertionSortRecursive(array, array.length);
         System.out.println(Arrays.toString(array));
     }
 

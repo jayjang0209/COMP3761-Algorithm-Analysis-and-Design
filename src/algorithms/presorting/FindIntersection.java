05398bf4aad24b7ff540b972b6356dbde6f9eefc
Jonghoon Jang <jayjang0209@gmail.com>
2022-02-21 21:44:02 -0800

    Add pre-sort algorithm for find intersection

--- src/algorithms/presorting/FindIntersection.java
+++ src/algorithms/presorting/FindIntersection.java
@@ -1,14 +1,38 @@
 package algorithms.presorting;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FindIntersection {
+
+    public static ArrayList<Integer> findIntersectionUsingPresort(int[] A, int[] B) {
+        Arrays.sort(A); // O(n*logn)
+        Arrays.sort(B);
+        ArrayList<Integer> intersection = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while (i != A.length && j != B.length) {
+            if (A[i] == B[j]) {
+                intersection.add(A[i]);
+                i++;
+                j++;
+            } else if (A[i] < B[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return intersection;
+    }
+
+
+
 
     public static ArrayList<Integer> findIntersectionUsingBruteForce(int[] A, int[] B) {
         ArrayList<Integer> intersection = new ArrayList<>();
         for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < A.length; j++) {
-                if (A[i] == B[j]) {
+            for (int j = 0; j < B.length; j++) {
+                if (A[i] == B[j]) { // O(n^2) basic operation
                     intersection.add(A[i]);
                 }
             }
@@ -20,8 +44,9 @@ public class FindIntersection {
 
 
     public static void main(String[] args) {
-        int[] A = {1, 2, 3, 4, 5, 6};
-        int[] B = {3, 4, 5, 6, 7, 8};
+        int[] A = {1, 2, 3, 5, 9, 8, 19};
+        int[] B = {3, 4, 5, 7, 8, 15, 11, 19};
         System.out.println(findIntersectionUsingBruteForce(A, B));
+        System.out.println(findIntersectionUsingPresort(A, B));
     }
 }

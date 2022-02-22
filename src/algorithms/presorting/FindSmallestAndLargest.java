7d9d4dd46d5dde314179e16fe317ccb202738fe2
Jonghoon Jang <jayjang0209@gmail.com>
2022-02-21 22:10:22 -0800

    Add Presort and BruteForce algo for Find smallest and Largest

---
+++ src/algorithms/presorting/FindSmallestAndLargest.java
@@ -0,0 +1,36 @@
+package algorithms.presorting;
+
+import java.util.Arrays;
+
+public class FindSmallestAndLargest {
+
+    public static int[] findSmallestAndLargestUsingBruteForce(int[] array) {
+        int[] smallestAndLargest = new int[2];
+        int smallest = array[0];
+        int largest = array[0];
+        for (int number : array) {
+            if (number < smallest) {
+                smallest = number;
+            } else if (number > largest) {
+                largest = number;
+            }
+        }
+        smallestAndLargest[0] = smallest;
+        smallestAndLargest[1] = largest;
+        return smallestAndLargest;
+    }
+
+    public static int[] findSmallestAndLargestUsingPresort(int[] array) {
+        Arrays.sort(array);
+        int[] smallestAndLargest = new int[2];
+        smallestAndLargest[0] = array[0];
+        smallestAndLargest[1] = array[array.length - 1];
+        return smallestAndLargest;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 5, 0, 11, 6, 9, -3};
+        System.out.println(Arrays.toString(findSmallestAndLargestUsingBruteForce(array)));
+        System.out.println(Arrays.toString(findSmallestAndLargestUsingPresort(array)));
+    }
+}

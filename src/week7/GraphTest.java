fdb77c12391aec4f60f40f5fd04eccf9cce7fb68
Jonghoon Jang <jayjang0209@gmail.com>
2022-03-14 21:14:29 -0700

    Test DFS and BFS

--- src/week7/GraphTest.java
+++ src/week7/GraphTest.java
@@ -35,6 +35,39 @@ public class GraphTest {
         int d2 = secondTestWithSizeFour.degree(0);
         System.out.println("degree[0] = " + d2);
 
+        // Part1 - Q4
+        System.out.println("\nPart1 - Q4");
+        AdjGraph testWithDirectedGraph = new AdjGraph(4);
+        testWithDirectedGraph.setDirected();
+        testWithDirectedGraph.addEdge(0, 1);
+        testWithDirectedGraph.addEdge(1, 2);
+        testWithDirectedGraph.addEdge(2, 3);
+        System.out.println("directed=" + testWithDirectedGraph.isDirected());
+
+        // Part2
+        AdjGraph testGraph = new AdjGraph(8);
+        testGraph.addEdge(0, 1);
+        testGraph.addEdge(0, 2);
+        testGraph.addEdge(0, 4);
+        testGraph.addEdge(1, 3);
+        testGraph.addEdge(1, 5);
+        testGraph.addEdge(2, 3);
+        testGraph.addEdge(2, 6);
+        testGraph.addEdge(3, 7);
+        testGraph.addEdge(4, 5);
+        testGraph.addEdge(4, 6);
+        testGraph.addEdge(5, 7);
+        testGraph.addEdge(6, 7);
+
+        // Part2 - Q1 - DFS
+        System.out.println("\nPart2 - Q1");
+        testGraph.DFS();
+
+        // Part2 - Q1 - DFS
+        System.out.println("\nPart2 - Q2");
+        testGraph.BFS();
+
+
 
     }
 }
